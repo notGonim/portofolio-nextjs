31243ad41a0a63436802eea102cebd3ac4ea2c57
notGonim <mhmodgonim96@gmail.com>
2021-08-30 22:50:04 -1000

    feat:Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,28 +4,54 @@ import { GoLocation } from "react-icons/go";
 import { GiTie } from "react-icons/gi";
 export const Sidebar = () => {
   return (
-    <div>
+    <div className="Poppins">
       <img
         src="https://pbs.twimg.com/profile_images/1424005296394018817/nWcSZdOU_400x400.jpg"
         alt="profile image"
+        className="w-32 h-32 mx-auto rounded-full "
       />
-      <h3>
-        <span>Mahmoud</span>
+      <h3 className="my-4 text-3xl font-medium tracking-wide text-blue-600">
         Gonim
       </h3>
-      <p>Frontend Engineer</p>
-      <p>Download Resume</p>
-      {/* Social Icons */}
-      {/* ADDRESS*/}
-      <div className="">
-        <div className="">
-          <span>Cairo,Egypt </span>
-        </div>
-        <p>mghonamie@gmail.com</p>
-        <p>+201062235196</p>
+      <p className="px-2 py-1 my-3 bg-gray-200 rounded-full">
+        Frontend Engineer
+      </p>
+      <a
+        className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full"
+        download="gonim"
+      >
+        <GiTie className="w-6 h-6" /> Download Resume
+      </a>
+      <div className="flex justify-around w-9/12 mx-auto my-5 text-blue-500 md:w-full">
+        <a href="">
+          <FiGithub className="w-8 h-8 cursor-pointer" />
+        </a>
+        <a href="">
+          <FiTwitch className="w-8 h-8 cursor-pointer" />
+        </a>
+        <a href="">
+          <FiLinkedin className="w-8 h-8 cursor-pointer" />
+        </a>
       </div>
-      <button>Email Me</button>
-      <button>Toggle Theme</button>
+      <div
+        className="py-4 my-4 bg-gray-200 "
+        style={{ marginLeft: "-1rem", marginRight: "-1rem" }}
+      >
+        <div className="flex items-end justify-center space-x-2">
+          <GoLocation /> <span>Cairo,Egypt </span>
+        </div>
+        <p className="my-2">mghonamie@gmail.com</p>
+        <p className="my-2">+201062235196</p>
+      </div>
+      <button
+        className="w-8/12 px-5 py-2 my-2 text-white bg-blue-800 rounded-full focus:outline-none"
+        onClick={() => window.open("mailto:mghonamie@gmail.com")}
+      >
+        Email Me
+      </button>
+      <button className="w-8/12 px-5 py-2 my-2 text-white bg-blue-800 rounded-full ">
+        Toggle Theme
+      </button>
     </div>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="grid-cols-12 gap-6 px-5 lg:px-48 my-14">
-      <div className="col-span-12 p-4 text-center bg-white rounded-2xl lg:col-span-1">
+    <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 sm:px-20 md:px-32 ">
+      <div className="col-span-12 p-4 text-center bg-white rounded-2xl lg:col-span-3 ">
         <Sidebar />
       </div>
       <div className="col-span-12 p-4 bg-white rounded-2xl lg:col-span-9">
